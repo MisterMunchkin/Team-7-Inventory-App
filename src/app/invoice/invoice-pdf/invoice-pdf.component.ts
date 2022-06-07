23bc610087b544a5e0b5b58e01dc910f8ef99fb3
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2022-06-07 23:03:59 +0800

    added better name for downloaded pdf: Invoice#<invoiceNumber>_<billTo>.pdf

--- src/app/invoice/invoice-pdf/invoice-pdf.component.ts
+++ src/app/invoice/invoice-pdf/invoice-pdf.component.ts
@@ -24,36 +24,6 @@ export class InvoicePdfComponent implements OnInit {
   ngOnInit() {
   }
 
-  // async downloadPDF() {
-  //   const invoiceHTML = this.invoiceHTML.nativeElement;
-  //   var html = htmlToPdfMake(invoiceHTML.innerHTML);
-  //   const documentDefinition = { content: html};
-  //   pdfMake.createPdf(documentDefinition).download();
-  // }
-
-  // downloadPDF() {
-  //   var data = document.getElementById('invoice');
-
-  //   if (data) {
-  //     html2canvas(data)
-  //     .then(canvas => {
-  //       if (data) {
-  //         var imgWidth = 208;
-  //         var imgHeight = canvas.height * imgWidth / canvas.width;
-
-  //         // var imgWidth = canvas.width;
-  //         // var imgHeight = canvas.height;
-
-  //         const contentDataURL = canvas.toDataURL('image/png');
-  //         let pdf = new jsPDF('p', 'mm', 'a4');
-  //         var position = 0;
-  //         pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
-  //         pdf.save('test.pdf');
-  //       }
-  //     });
-  //   }
-  // }
-
   downloadPDF() {
     var data = document.getElementById('invoice');
 
@@ -64,7 +34,7 @@ export class InvoicePdfComponent implements OnInit {
 
         pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, pdf.internal.pageSize.width, pdf.internal.pageSize.height);
 
-        pdf.save('test.pdf');
+        pdf.save('Invoice#' + this.invoiceData.invoiceNumber + '_' + this.invoiceData.billTo + '.pdf');
       });
     }
   }
